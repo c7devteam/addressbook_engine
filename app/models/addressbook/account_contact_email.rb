07a2dfc77d2e8219ccdfbd2a809b48c6efaf53ea
robertomiranda <rjmaltamar@gmail.com>
2014-03-20 19:35:34 -0500

    Indexing AccountContactEmail

--- app/models/addressbook/account_contact_email.rb
+++ app/models/addressbook/account_contact_email.rb
@@ -1,6 +1,8 @@
 module Addressbook
   class AccountContactEmail < ActiveRecord::Base
     #attr_accessible :account_contact_id, :deleted_at, :email, :preferred
+    include Tire::Model::Search
+    include Tire::Model::Callbacks
     acts_as_paranoid
 
     #attr_accessible :email, :preferred
@@ -10,6 +12,27 @@ module Addressbook
     belongs_to :account_contact
 
     delegate :account, to: :account_contact
+    delegate :contact_groups, to: :account_contact
+    delegate :phones, to: :account_contact
+    delegate :first_letter, to: :account_contact
+
+    mapping do
+      indexes :id, type: "integer", index: :not_analyzed
+      indexes :email
+
+      indexes :account_contact_name, type: 'string'
+      indexes :contact_groups, type: 'string'
+      indexes :phones, type: 'string'
+      indexes :first_letter
+    end
+
+    def to_indexed_json
+      to_json(methods: [:account_contact_name])
+    end
+
+    def account_contact_name
+      account_contact.fullname
+    end
 
     def to_s
       "#{account_contact.to_s}(#{email})"
